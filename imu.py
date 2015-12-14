fa8cea083211b32ab2fe63edfe4dd78ab0a03ea9
przemek29 <przemyslawwojcik91@gmail.com>
2015-12-14 11:29:18 +0000

    Refactoring of imu.py file

--- imu.py
+++ imu.py
@@ -13,12 +13,14 @@ class IMU(object):
     
     K = 0.98
     K1 = 1 - K
+   
+
+
     
-    def __init__(self, bus, gyro_address, accel_address, compass_address, baro_address, box_address, name, gyro_scale=L3G4200D.FS_2000, accel_scale=ADXL345.AFS_16g):
+    def __init__(self, bus, gyro_address, accel_address, compass_address, box_address, name, gyro_scale=L3G4200D.FS_2000, accel_scale=ADXL345.AFS_16g):
         self.bus = bus
         self.gyro_address = gyro_address 
         self.accel_address = accel_address
-        self.baro_address = baro_address
         self.box_address = box_address
         self.name = name
         self.gyro_scale = gyro_scale 
@@ -26,7 +28,6 @@ class IMU(object):
         self.box = MPU6050(bus, box_address, name + "-box")
         self.accelerometer = ADXL345(bus, accel_address, name + "-accelerometer", accel_scale)
         self.gyroscope = L3G4200D(bus, gyro_address, name + "-gyroscope", gyro_scale)
-        self.barometer = BMP085(bus, baro_address, name + "-barometer")
         self.compass = HMC5883L(bus, compass_address, name + "-compass")
 
         self.last_time = time.time()
@@ -70,45 +71,13 @@ class IMU(object):
         self.accel_1_scaled_z = round(self.box.read_scaled_accel_z(),4)
         
         
-        now = time.time()
-        self.time_diff = now - self.last_time
-        self.last_time = now 
-        (self.pitch, self.roll) = self.comp_filter(self.rotation_x, self.rotation_y)
         
         return str(self.gyro_scaled_x) +","+ str(self.gyro_scaled_y)+","+str(self.gyro_scaled_z)+ "," + \
                str(self.accel_scaled_x)+ "," +str(self.accel_scaled_y)+ ","+ \
                str(self.accel_scaled_z)+ ","+str(self.gyro_1_scaled_x)+ ","+ str(self.gyro_1_scaled_y)+ ","+\
                str(self.gyro_1_scaled_z)+ ","+ str(self.accel_1_scaled_x)+ ","+ str(self.accel_1_scaled_y)+ ","+ \
                str(self.accel_1_scaled_z)
-  	#return str(self.pitch)+","+str(self.roll)+",""1"  
 
-    def read_x_rotation(self, x, y, z):
-        return self.rotation_x
-
-    def read_y_rotation(self, x, y, z):
-        return self.rotation_y
-
-    def comp_filter(self, current_x, current_y):
-        new_pitch = IMU.K * (self.pitch + self.gyro_scaled_x * self.time_diff) + (IMU.K1 * current_x)
-        new_roll = IMU.K * (self.roll + self.gyro_scaled_y * self.time_diff) + (IMU.K1 * current_y)
-        return (new_pitch, new_roll)
-
-
-    def read_pitch_roll_yaw(self):
-        '''
-        Return pitch, roll and yaw in radians
-        '''
-        (raw_pitch, raw_roll, self.gyro_scaled_x, self.gyro_scaled_y, \
-            self.gyro_scaled_z, self.accel_scaled_x, self.accel_scaled_y, \
-            self.accel_scaled_z) = self.read_all()
-        
-        now = time.time()
-        self.time_diff = now - self.last_time
-        self.last_time = now 
-        
-        (self.pitch, self.roll) = self.comp_filter(raw_pitch, raw_roll)
-        self.yaw = self.compass.read_compensated_bearing(self.pitch, self.roll)
-        
        # return str(int(self.pitch))+","+ str(int(self.roll))+","+ str(int(self.yaw))
 	return (self.pitch, self.roll, self.yaw)	
 
@@ -117,27 +86,15 @@ class IMU(object):
 
 if __name__ == "__main__":
     bus = smbus.SMBus(1)#i2c_raspberry_pi_bus_number())
-    imu_controller = IMU(bus, 0x69, 0x53, 0x1e, 0x77, 0x68, "IMU")
+    imu_controller = IMU(bus, 0x69, 0x53, 0x1e, 0x68, "IMU")
     i = 0
     imu_controller.set_compass_offsets(9, -10, -140)
     hmc5883l = HMC5883L(bus, 0x1e, "magneto" )
-    #(pitch, roll, yaw) = imu_controller.read_pitch_roll_yaw()
-    print "Packet number, Gyroscope X (deg/s), Gyroscope Y (deg/s), Gyroscope Z, Accelometer X (g), Accelerometer Y (g), Accelerometer Z (g), Magnetometer X (G), Magnetometer Y (G), Magnetometer Z (G)"
+
     while True:
-    #for i in range(0,100):
-        #start = timeit.timeit()
 	data = imu_controller.read_all()
 	magneto = hmc5883l.read_all()
-#	katy = imu_controller.read_pitch_roll_yaw()
-#	(pitch, roll, yaw) = imu_controller.read_pitch_roll_yaw()
-#	result = "%d, %d, %d" % (int(pitch *90)-8, int(roll*90)+8, int(yaw*90)-8)+ data
-        #result = "%.2f %.2f %.2f" % (pitch, roll, yaw)
-        #end = timeit.timeit()
-	#roznica = end - start
-	#print str(i),data
-        #print data + "\n"
-#	print result
-	print str(i) + "," + str(data) + "," + str(magneto)
+	print str(i) + "," + str(data) + str(magneto)
 	i+=1
 	
  
