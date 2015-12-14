56ca03cf595ebd23ee6861ec0ebf5e8ea2083676
przemek29 <przemyslawwojcik91@gmail.com>
2015-12-14 13:58:28 +0000

    Testing of serial port communication

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-
 from imu import IMU
 from bmp085 import BMP085
 import smbus
 import math
+import serial
 
+#ser = serial.Serial(
+    #port='/dev/ttyAMA0',
+    #baudrate=115200,
+    #parity=serial.PARITY_SPACE,
+    #stopbits=serial.STOPBITS_ONE,
+    #bytesize=serial.SEVENBITS
+#)
+
+#ser.open()
+#ser.isOpen()
+	
 if __name__ == "__main__":
+    
+    
 
     bus = smbus.SMBus(1)
     imu_controller = IMU(bus, 0x69, 0x53, 0x1e, 0x68, "IMU")
@@ -13,10 +26,10 @@ if __name__ == "__main__":
 
     while True:
 	imu_data = imu_controller.read_all()
-	if not (counter%125):
-	    barometer = BMP085(bus, 0x77, "-barometer")
-   	    pressure = barometer.read_pressure()
-	    temperature = barometer.read_temperature()
+	#if not (counter%125):
+	    #barometer = BMP085(bus, 0x77, "-barometer")
+   	    #pressure = barometer.read_pressure()
+	    #temperature = barometer.read_temperature()
 
 	print str(counter) + "," + str(imu_data)
 	#print str(counter) + "," + str(imu_data) + "," + str(pressure) + "," + str(temperature) 
